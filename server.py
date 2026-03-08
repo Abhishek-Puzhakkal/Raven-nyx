11c0680fe5586492c24975b57185acdf52786912
Abhishek Puzhakkal <abhizgc@gmail.com>
2026-03-08 15:39:31 +0100

    debugging

--- server.py
+++ server.py
@@ -243,8 +243,8 @@ class GroupChatServer:
     def gp_chat_close(self):
         for clients in self.clients_socket_session_key_mapping:
             clients.close()
-            self.clients_socket_session_key_mapping.pop(clients)
         self.gp_chat_svr_socket.close()
+        self.clients_socket_session_key_mapping.clear()
         print('entire connection closed peacefully....')
 
 
