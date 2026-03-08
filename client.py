e9ca75f88d498f9cbef5ab5e3cdb8f8973b8e805
Abhishek Puzhakkal <abhizgc@gmail.com>
2026-03-08 10:37:51 +0100

    implemented NN Pattern handshake from noiceprotocol

--- client.py
+++ client.py
@@ -1,10 +1,9 @@
 import socket
-from cryptography.fernet import Fernet
+from noise.connection import NoiseConnection
 
 class Client:
     def __init__(self, ip, port, username):
-        self.key = b'l3d9jKgRTKAMWazsSKBsKmTAZ3x9RfeUdfi3zKbmDPI='
-        self.cryptography = Fernet(self.key)
+        self.proto = None
         self.username = username[0]
         self.server_addr = ip[0]
         self.port = port[0]
@@ -21,6 +20,15 @@ class Client:
 
             self.client_socket.connect((self.server_addr, self.port))
             print(f'client connected to {self.server_addr}')
+            print('handshake starting....')
+            self.proto = NoiseConnection.from_name(b'Noise_NN_25519_ChaChaPoly_SHA256')
+            self.proto.set_as_initiator()
+            self.proto.start_handshake()
+            message = self.proto.write_message()
+            self.client_socket.sendall(message)
+            received = self.client_socket.recv(2048)
+            payload = self.proto.read_message(received)
+
             
             return True
         except KeyboardInterrupt:
@@ -56,7 +64,7 @@ class Client:
                 
                 self.clinet_message = self.username + ' : '+ message
 
-                self.client_socket.sendall(self.cryptography.encrypt(self.clinet_message.encode()))
+                self.client_socket.sendall(self.proto.encrypt(self.clinet_message.encode()))
                 
 
         
@@ -83,7 +91,7 @@ class Client:
                 break
 
             if servermessage:
-                self.server_message = self.cryptography.decrypt(servermessage).decode()
+                self.server_message = self.proto.decrypt(servermessage).decode()
                 
             
                 if not self.running:
@@ -114,11 +122,19 @@ class GpChatClient:
         self.client_gp_chat_socket = socket.socket()
         self.client_running = True
         self.quit_checker = list()
-        self.gp_cht_cryptography_ky = b'2b1gSNyIH1g3-huR0gAHcuCZK1mFURW46xiuWsEnw_M='
-        self.gp_cryptography_object = Fernet(self.gp_cht_cryptography_ky)
+        self.proto = None
+        '''self.gp_cht_cryptography_ky = b'2b1gSNyIH1g3-huR0gAHcuCZK1mFURW46xiuWsEnw_M='
+        self.gp_cryptography_object = Fernet(self.gp_cht_cryptography_ky)'''
     def client_gp_chat_connection(self):
         try :
             self.client_gp_chat_socket.connect((self.addr, self.port))
+            self.proto = NoiseConnection.from_name(b'Noise_NN_25519_ChaChaPoly_SHA256')
+            self.proto.set_as_initiator()
+            self.proto.start_handshake()
+            message = self.proto.write_message()
+            self.client_gp_chat_socket.sendall(message)
+            received = self.client_gp_chat_socket.recv(2048)
+            payload = self.proto.read_message(received)
             return True
         except Exception as e :
             
@@ -134,12 +150,12 @@ class GpChatClient:
                 msg = self.uername + ' : ' + message
                 
                 if message == 'quit':
-                    self.client_gp_chat_socket.sendall(self.gp_cryptography_object.encrypt(msg.encode()))
+                    self.client_gp_chat_socket.sendall(self.proto.encrypt(msg.encode()))
                     print('you enterd the "quit", so conection terminating....')
                     self.client_running = False
                     break
 
-                self.client_gp_chat_socket.sendall(self.gp_cryptography_object.encrypt(msg.encode()))
+                self.client_gp_chat_socket.sendall(self.proto.encrypt(msg.encode()))
         except KeyboardInterrupt:
             print('keyboard intrepted ....')
         except Exception as e :
@@ -161,7 +177,7 @@ class GpChatClient:
                 if not self.client_running:
                     break
                 
-                decrypted_msg = self.gp_cryptography_object.decrypt(message).decode()
+                decrypted_msg = self.proto.decrypt(message).decode()
 
                 self.quit_checker = decrypted_msg.split()
                 
--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 import socket
-from cryptography.fernet import Fernet
+from noise.connection import NoiseConnection
+from itertools import cycle
 import threading
 
 
 class Server:
     def __init__(self, port, username):
-        self.key = b'l3d9jKgRTKAMWazsSKBsKmTAZ3x9RfeUdfi3zKbmDPI='
-        self.cryptography = Fernet(self.key)
+        self.proto = None
         self.user_name = username[0]
         self.port = port[0]
         self.server = socket.socket()
@@ -24,6 +24,20 @@ class Server:
             self.server.listen()
 
             self.client, clinet_addr = self.server.accept()
+
+            self.proto = NoiseConnection.from_name(b'Noise_NN_25519_ChaChaPoly_SHA256')
+            self.proto.set_as_responder()
+            self.proto.start_handshake()
+
+            for action in cycle(['receive', 'send']):
+                if self.proto.handshake_finished:
+                    break
+                elif action == 'send':
+                    ciphertext = self.proto.write_message()
+                    self.client.sendall(ciphertext)
+                elif action == 'receive':
+                    data = self.client.recv(2048)
+                    plaintext = self.proto.read_message(data)
 
             return self.client, clinet_addr
         
@@ -43,11 +57,11 @@ class Server:
             
             if message == 'quit':
                 
-                self.client.sendall(self.cryptography.encrypt(self.serv_message.encode()))
+                self.client.sendall(self.proto.encrypt(self.serv_message.encode()))
                 print('\nyou entered quit , the connection is terminating......', flush=True)
                 self.running = False
                 break
-            self.client.sendall(self.cryptography.encrypt(self.serv_message.encode()))
+            self.client.sendall(self.proto.encrypt(self.serv_message.encode()))
         
         
     def server_recv_msg(self):
@@ -67,7 +81,7 @@ class Server:
             if not self.running:
                 break
 
-            client_message_decrypted = self.cryptography.decrypt(client_message).decode()
+            client_message_decrypted = self.proto.decrypt(client_message).decode()
             self.quit_checker = list(client_message_decrypted.split())
             if len(self.quit_checker) == 3 and self.quit_checker[2] == 'quit':
                 self.running = False
@@ -90,10 +104,11 @@ class GroupChatServer:
         self.clients_socket = None
         self.clients_addr = None
         self.server_running = True
-        self.clients_dict = dict()
+        '''self.clients_dict = dict()'''
         self.quict_checker = list()
-        self.gp_cht_cryptography_ky = b'2b1gSNyIH1g3-huR0gAHcuCZK1mFURW46xiuWsEnw_M='
-        self.gp_cryptography_object = Fernet(self.gp_cht_cryptography_ky)
+        '''self.gp_cht_cryptography_ky = b'2b1gSNyIH1g3-huR0gAHcuCZK1mFURW46xiuWsEnw_M='
+        self.gp_cryptography_object = Fernet(self.gp_cht_cryptography_ky)'''
+        self.clients_socket_session_key_mapping = dict()
     def connection(self):
         self.gp_chat_svr_socket.bind(('0.0.0.0', self.port))
         self.gp_chat_svr_socket.listen()
@@ -121,8 +136,8 @@ class GroupChatServer:
                         break
                 
                     
-                    
-                    decrypted_client_msg = self.gp_cryptography_object.decrypt(client_message).decode()
+                    session_key_client = self.clients_socket_session_key_mapping[client]
+                    decrypted_client_msg = session_key_client.decrypt(client_message).decode()
                     
                     broadcasting_flag = 'broadcasting'
                     
@@ -133,26 +148,26 @@ class GroupChatServer:
                             broadcasting_message = broadcasting_flag + ' ' + decrypted_client_msg
                             print(f'\n{decrypted_client_msg}')
                             print(f'\n{username} quit, so that connection is closing...')
-                            for clients in self.clients_dict:
+                            for clients, session_key in self.clients_socket_session_key_mapping.items():
                                 if clients == sender:
                                     pass
                                 else:
-                                    clients.sendall(self.gp_cryptography_object.encrypt(broadcasting_message.encode()))
+                                    clients.sendall(session_key.encrypt(broadcasting_message.encode()))
                             
                             client.close()
-                            self.clients_dict.pop(client)
-                            print(f' \nnow the remaining members in group chat : {len(self.clients_dict)}')
+                            self.clients_socket_session_key_mapping.pop(client)
+                            print(f' \nnow the remaining members in group chat : {len(self.clients_socket_session_key_mapping)}')
                             break
                         
                         else:
                               
                             print(f'\n{decrypted_client_msg}')
                             broadcasting_message = broadcasting_flag + ' ' + decrypted_client_msg
-                            for clients in self.clients_dict:
+                            for clients, session_key in self.clients_socket_session_key_mapping.items():
                                 if clients == sender:
                                     pass
                                 else:
-                                    clients.sendall(self.gp_cryptography_object.encrypt(broadcasting_message.encode()))
+                                    clients.sendall(session_key.encrypt(broadcasting_message.encode()))
                                     
                     elif decrypted_client_msg and len(self.clients_dict) < 2:
                         self.quict_checker = decrypted_client_msg.split()
@@ -180,9 +195,25 @@ class GroupChatServer:
                 else:
                     raise
             if clients_socket:
-                if clients_socket not in self.clients_dict:
+                if clients_socket not in self.clients_socket_session_key_mapping:
                     print(f'\nnew connection arrived , {clients_addr} ')
-                    self.clients_dict[clients_socket] = clients_addr
+                    print('hand shake initialising....')
+                    noise = NoiseConnection.from_name(b'Noise_NN_25519_ChaChaPoly_SHA256')
+                    noise.set_as_responder()
+                    noise.start_handshake()
+
+                    # Perform handshake. Break when finished
+                    for action in cycle(['receive', 'send']):
+                        if noise.handshake_finished:
+                            break
+                        elif action == 'send':
+                            ciphertext = noise.write_message()
+                            clients_socket.sendall(ciphertext)
+                        elif action == 'receive':
+                            data = clients_socket.recv(2048)
+                            plaintext = noise.read_message(data)
+                    
+                    self.clients_socket_session_key_mapping[clients_socket] = noise
                     threading.Thread(target=server_brodcast, args=(clients_socket,)).start()
                     
     def gp_srvr_snt_msg(self):
@@ -196,13 +227,14 @@ class GroupChatServer:
                 
                 if server_message == 'quit':
                     self.server_running = False
-                    for clients in self.clients_dict:
-                        clients.sendall(self.gp_cryptography_object.encrypt(server_message_broadcast.encode()))
+                    for clients, session_key in self.clients_socket_session_key_mapping.items():
+                        
+                        clients.sendall(session_key.encrypt(server_message_broadcast.encode()))
                     print('you enterd quit , connection going to terminate....')
                     
                     break
-                for clients in self.clients_dict:
-                    clients.sendall(self.gp_cryptography_object.encrypt(server_message_broadcast.encode()))
+                for clients, session_key in self.clients_socket_session_key_mapping.items():
+                    clients.sendall(session_key.encrypt(server_message_broadcast.encode()))
         except KeyboardInterrupt:
             print('keyboard interpted....')
         except Exception as e:
