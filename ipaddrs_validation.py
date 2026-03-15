435e1f1923c82dda370e7f35084dfc27f632d8c2
Abhishek Puzhakkal <abhizgc@gmail.com>
2026-03-15 18:30:26 +0100

    debugging

--- ipaddrs_validation.py
+++ ipaddrs_validation.py
@@ -1,14 +1,11 @@
 import ipaddress
 
 class IpAddressValidation :
-    def __init__(self, private_ip : list):
-        self.ip = private_ip
 
-    def validation(self) -> bool:
+    def validation(self, ip ) -> bool:
 
         try:
-
-            return ipaddress.ip_address(self.ip).is_private
+            return ipaddress.ip_address(ip).is_private
         except ValueError:
-            pass
+            raise
         
