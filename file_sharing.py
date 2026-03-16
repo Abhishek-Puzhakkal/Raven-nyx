bbb4dc97b672fa2034645a9423fde5b416b49260
Abhishek Puzhakkal <abhizgc@gmail.com>
2026-03-16 12:52:08 +0100

    added tor based one to one file sharing feature

--- file_sharing.py
+++ file_sharing.py
@@ -7,6 +7,9 @@ from server import RecvExactBytes
 import hashlib
 from itertools import cycle
 import json
+from stem.control import Controller
+import socks
+
 
 class File_sender():
     def __init__(self, addr, port, file_path):
@@ -227,6 +230,264 @@ class LanFileReceiver():
             self.file_recevier_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
             self.file_recevier_socket.bind(('0.0.0.0', self.port))
+
+            self.file_recevier_socket.listen()
+
+            client_socket , addr = self.file_recevier_socket.accept()
+
+            print(f'\n{addr} connected successfully...\n handshake initialising...')
+
+            self.session_key.set_as_responder()
+            self.session_key.start_handshake()
+
+            for action in cycle(['receive', 'send']):
+                if self.session_key.handshake_finished:
+                    break
+                elif action == 'send':
+                    ciphertext = self.session_key.write_message()
+                    ciphertext_size_header = len(ciphertext).to_bytes(4,'big')
+                    client_socket.sendall(ciphertext_size_header + ciphertext)
+                elif action == 'receive':
+                    recv_exact_byte = RecvExactBytes()
+
+                    received_message_header = recv_exact_byte.recv_exact_bytes(client_socket, 4)
+                    received_message_size = int.from_bytes(received_message_header, 'big')
+
+                    received_message = recv_exact_byte.recv_exact_bytes(client_socket, received_message_size)    
+
+                    plaintext = self.session_key.read_message(received_message)                
+
+            print(f'\nhandshake finished...')
+
+            file_header_size = recv_exact_byte.recv_exact_bytes(client_socket, 2)
+
+            encrypted_file_header = recv_exact_byte.recv_exact_bytes(client_socket, int.from_bytes(file_header_size,'big'))
+
+            file_header = json.loads(self.session_key.decrypt(encrypted_file_header).decode())
+
+            file_size = file_header['file_size']
+            
+            checksum = file_header['checksum']
+            print(f'\nsender given metadata  file_size_in_bytes : { file_size}   checksum = {checksum}')
+
+            if file_size and checksum:
+                response_flag = self.session_key.encrypt('FILE_HEADER_GOT_SUCCESSFULLY'.encode())
+
+                size_response_flag = len(response_flag).to_bytes(2, 'big')
+
+                client_socket.sendall(size_response_flag + response_flag)
+
+                with open(file_path, 'wb') as file:
+
+                    received_chunk_size = 0
+
+                    p_bar = tqdm(total=file_size, unit='B', unit_scale=True, desc="Receiving")
+
+                    while received_chunk_size < file_size:
+
+                        chunk_size = recv_exact_byte.recv_exact_bytes(client_socket, 4)
+
+                        encrypted_chunk = recv_exact_byte.recv_exact_bytes(client_socket, int.from_bytes(chunk_size, 'big'))
+                        decrypted_chunk = self.session_key.decrypt(encrypted_chunk)
+                        
+
+                        if not encrypted_chunk:
+                            break
+
+
+                        file.write(decrypted_chunk)
+
+                        received_chunk_size += len(decrypted_chunk)
+
+                        p_bar.update(len(decrypted_chunk))
+                    
+                    p_bar.close()
+                
+                print('\nfile received ...\n Recalculating checksum of received file ...')
+
+                sha256_hash = hashlib.sha256()
+
+                with open(file_path, 'rb') as file:
+                    while True:
+                        data = file.read(65536)
+                        if not data:
+                            break
+                        
+                        sha256_hash.update(data)
+
+                if sha256_hash.hexdigest() == checksum:
+
+                    print(f'\nRecalculated_checksum :- {sha256_hash.hexdigest()}\nChecksum matches')
+                    print('File received successfully...')
+                    response_flag = self.session_key.encrypt('FILE_DOWNLOADED_SUCCESSFULLY'.encode())
+                    response_flag_size = len(response_flag).to_bytes(2, 'big')
+                    client_socket.sendall(response_flag_size + response_flag)
+                    client_socket.close()
+                    self.file_recevier_socket.close()
+                else:
+                    response_flag = self.session_key.encrypt('FILE_DOWNLOADED_UNSUCCESSFULLY'.encode())
+                    response_flag_size = len(response_flag).to_bytes(2, 'big')
+                    client_socket.sendall(response_flag_size + response_flag)
+                    print('\nSomthing went wrong... \n Checksum missmatch...')
+                    print(f'\nsender given checksum :- {checksum}')
+                    print(f'\nreceived file checksum :- {sha256_hash.hexdigest()} \n please resume the program ')
+                    client_socket.close()
+                    self.file_recevier_socket.close()
+
+class TorFilesender():
+    def __init__(self, file_path, onion_addr):
+        self.file_path = file_path[0]
+        self.recevier_addr = onion_addr[0]
+        self.file_sender_socket = socks.socksocket()
+        self.session_key =  NoiseConnection.from_name(b'Noise_NN_25519_ChaChaPoly_SHA256')
+
+    
+    def send_file(self):
+        
+        file_path = Path(self.file_path).expanduser()
+
+        if file_path.exists():
+
+            self.file_sender_socket.set_proxy(socks.SOCKS5, '127.0.0.1', 9050)
+            self.file_sender_socket.connect((self.recevier_addr, 80))
+            print('\nconnection successfull... \n handshake starting....')
+            self.session_key.set_as_initiator()
+            self.session_key.start_handshake()
+            message = self.session_key.write_message()
+            message_size_header = len(message).to_bytes(4,'big')
+            self.file_sender_socket.sendall(message_size_header + message)
+            recv_exact_byte = RecvExactBytes()
+
+            received_message_header = recv_exact_byte.recv_exact_bytes(self.file_sender_socket, 4)
+            received_message_size = int.from_bytes(received_message_header, 'big')
+
+            received_message = recv_exact_byte.recv_exact_bytes(self.file_sender_socket, received_message_size)
+
+            payload = self.session_key.read_message(received_message)
+
+            print(f'\nhandshake finished...\n creating checksum of   {self.file_path}')
+            sha256_hash = hashlib.sha256()
+
+            with open(file_path, 'rb') as file:
+                while True:
+                    data = file.read(65536)
+                    if not data:
+                        break
+                    
+                    sha256_hash.update(data)
+            
+            print(f'\nchecksum :-  {sha256_hash.hexdigest()}')
+
+            file_size = file_path.stat().st_size
+
+            file_sending_header = {
+
+                'file_size' : file_size,
+                'checksum' : sha256_hash.hexdigest()
+
+            }
+
+            encrypted_file_sending_header = self.session_key.encrypt(json.dumps(file_sending_header).encode())
+            len_encrypted_file_sending_header = len(encrypted_file_sending_header).to_bytes(2, 'big')
+
+            self.file_sender_socket.sendall(len_encrypted_file_sending_header + encrypted_file_sending_header)
+            
+            response_flag_size = recv_exact_byte.recv_exact_bytes(self.file_sender_socket, 2)
+
+            response_flag_size_int_form = int.from_bytes(response_flag_size,'big')
+
+            response_flag = recv_exact_byte.recv_exact_bytes(self.file_sender_socket, response_flag_size_int_form)
+
+            if 'FILE_HEADER_GOT_SUCCESSFULLY' == self.session_key.decrypt(response_flag).decode():
+
+                print('\nfile sending...')
+
+                with open(file_path, 'rb') as file:
+                    p_bar = tqdm(total=file_size, unit='B', unit_scale=True, desc='Sending')
+
+                    while chunk := file.read(64 * 1024):
+                        encrypted_chunk = self.session_key.encrypt(chunk)
+                        encrypted_chunk_size = len(encrypted_chunk).to_bytes(4, 'big')
+                        self.file_sender_socket.sendall(encrypted_chunk_size + encrypted_chunk)
+                        p_bar.update(len(chunk))
+                    p_bar.close()
+                        
+                print('\nfile sending completed ...')
+
+                print('\nwaiting for client verification...')
+                response_flag_size = recv_exact_byte.recv_exact_bytes(self.file_sender_socket, 2)
+
+                response_flag_size_int_form = int.from_bytes(response_flag_size,'big')
+
+                response_flag = recv_exact_byte.recv_exact_bytes(self.file_sender_socket, response_flag_size_int_form)
+
+                if 'FILE_DOWNLOADED_SUCCESSFULLY' == self.session_key.decrypt(response_flag).decode():
+                    print('\nclient verification successfull, file sended and recevied successfully ...')
+                    self.file_sender_socket.close()
+                else:
+                    print('\nsomething went wrong...')
+                    print('\nclient failed while verifying the checksum, checksum missmatch occured ... \n please repeat the filesharing..')
+                    self.file_sender_socket.close()
+            
+            else:
+                print('\nsomething went wrong...')
+                self.file_sender_socket.close()
+        else:
+            print(f'\n{self.file_path} not exists...')
+
+
+class TorFileReceiver():
+    def __init__(self, file_path, port):
+        self.key_path = Path('.onion_key.txt')
+        self.tor_controller = Controller.from_port()
+        self.file_path = file_path[0]
+        self.port = port[0]
+        self.file_recevier_socket = socket.socket()
+        self.session_key = NoiseConnection.from_name(b'Noise_NN_25519_ChaChaPoly_SHA256')
+
+    def recv_file(self):
+        
+        file_path = Path(self.file_path).expanduser()
+
+        if file_path.exists():
+
+            user_choice = input(f'{self.file_path} alredy exists, you need to continue , type "yes" or "no" : ').lower()
+
+            if user_choice != 'yes' and user_choice != 'no':
+                print('invalid input')
+            elif user_choice == 'no':
+                print('please run the command once again with valid file path')
+            elif user_choice == 'yes':
+                print('okay then you need to rewrite the file or append to the file ?')
+                print('for rewriting the file just type : R \n for appending the file just type : A')
+                
+                user_choice = input(' : ').upper()
+
+                if user_choice != 'R' and user_choice != 'A':
+                    print('invalid input')
+                
+                elif user_choice == 'R':
+                    pass
+                else:
+                    pass
+        else:
+
+            self.tor_controller.authenticate()
+            if not self.key_path.exists():
+                service = self.tor_controller.create_ephemeral_hidden_service({80: 5000}, await_publication = True)
+                print("Started a new hidden service with the address of %s.onion" % service.service_id)
+
+                with open(self.key_path, 'w') as key_file:
+                    key_file.write('%s:%s' % (service.private_key_type, service.private_key))
+            else:
+                with open(self.key_path) as key_file:
+                    key_type, key_content = key_file.read().split(':', 1)
+
+                service = self.tor_controller.create_ephemeral_hidden_service({80: 5000}, key_type = key_type, key_content = key_content, await_publication = True)
+                print("Resumed %s.onion" % service.service_id)
+
+            self.file_recevier_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.file_recevier_socket.bind(('127.0.0.1', 5000))
 
             self.file_recevier_socket.listen()
 
